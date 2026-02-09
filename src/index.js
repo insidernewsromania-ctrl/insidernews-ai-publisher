87d91341a22af82f4052c3bfa6dec450562e34d0
insidernewsromania-ctrl <insidernewsromania@gmail.com>
2026-02-09 15:37:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,49 +4,36 @@ import { publishPost, uploadImage } from "./wordpress.js";
 import { downloadImage } from "./image.js";
 import { isDuplicate, saveTopic } from "./history.js";
 
-const BATCH_SIZE = 3;
+const BATCH_SIZE = 3; // MAXIM per rulare
 
 const categories = [
-  { name: "politica", id: 4058, weight: 40 },
-  { name: "social", id: 4063, weight: 25 },
-  { name: "economie", id: 4064, weight: 15 },
-  { name: "externe", id: 4060, weight: 20 }
+  { name: "politica", id: 4058 },
+  { name: "social", id: 4063 },
+  { name: "economie", id: 4064 },
+  { name: "externe", id: 4060 }
 ];
-
-function pickCategory() {
-  const total = categories.reduce((s, c) => s + c.weight, 0);
-  let rand = Math.random() * total;
-  for (const c of categories) {
-    if (rand < c.weight) return c;
-    rand -= c.weight;
-  }
-}
 
 async function run() {
   console.log("START SCRIPT");
 
-  for (let i = 1; i <= BATCH_SIZE; i++) {
-    const cat = pickCategory();
+  for (let i = 0; i < BATCH_SIZE; i++) {
+    const cat = categories[Math.floor(Math.random() * categories.length)];
 
     if (isDuplicate(cat.name)) continue;
 
     const title = await generateDiscoverHeadline(cat.name);
     const article = await generateArticle(cat.name);
 
+    article.title = title;
+
     await downloadImage(article.focus_keyword);
     const imageId = await uploadImage();
 
-    await publishPost(
-      {
-        title,
-        ...article
-      },
-      cat.id,
-      imageId
-    );
+    await publishPost(article, cat.id, imageId);
 
     saveTopic(cat.name);
-    await new Promise(r => setTimeout(r, 15000));
+
+    await new Promise(r => setTimeout(r, 20000));
   }
 
   console.log("DONE");
