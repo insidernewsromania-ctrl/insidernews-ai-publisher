6898c91e2d9121d73a6ca13db73a22905ea3734d
insidernewsromania-ctrl <insidernewsromania@gmail.com>
2026-02-09 15:34:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { generateArticle } from "./generator.js";
+import { generateDiscoverHeadline } from "./headline.js";
 import { publishPost, uploadImage } from "./wordpress.js";
 import { downloadImage } from "./image.js";
 import { isDuplicate, saveTopic } from "./history.js";
@@ -15,7 +16,6 @@ const categories = [
 function pickCategory() {
   const total = categories.reduce((s, c) => s + c.weight, 0);
   let rand = Math.random() * total;
-
   for (const c of categories) {
     if (rand < c.weight) return c;
     rand -= c.weight;
@@ -28,22 +28,24 @@ async function run() {
   for (let i = 1; i <= BATCH_SIZE; i++) {
     const cat = pickCategory();
 
-    if (isDuplicate(cat.name)) {
-      console.log("SKIP DUPLICATE:", cat.name);
-      continue;
-    }
+    if (isDuplicate(cat.name)) continue;
 
-    console.log(`Generating ${i}/${BATCH_SIZE} → ${cat.name}`);
-
+    const title = await generateDiscoverHeadline(cat.name);
     const article = await generateArticle(cat.name);
 
     await downloadImage(article.focus_keyword);
     const imageId = await uploadImage();
 
-    await publishPost(article, cat.id, imageId);
+    await publishPost(
+      {
+        title,
+        ...article
+      },
+      cat.id,
+      imageId
+    );
 
     saveTopic(cat.name);
-
     await new Promise(r => setTimeout(r, 15000));
   }
 
